dc5cb73f07b4fb141730d1511c3943ea0c69b82d
mathlete01 <msallin@gmail.com>
2020-11-17 07:06:51 -0800

    commit tree cdb3a00fd9b3aa5b10f6bec0c923b5bd1af0f860

--- index.js
+++ index.js
@@ -10,4 +10,24 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string){
+  // debugger
+  let arr = string.split(":");
+  let hourStr = arr[0];
+  let hour = parseInt(hourStr);
+  if (hour < 11){
+    return "Good Morning";
+  } else if (hour > 12 && hour < 17){
+    return "Good Afternoon"
+  } else if (hour > 17){
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string){
+  document.getElementById('greeting').innerText = string
+  // Below also works
+  // let element = document.querySelector("#greeting"); 
+  // element.innerText = string;
+}
